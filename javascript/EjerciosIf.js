8b6f19352f00bffb1e6e803ea9be179adb0b51f6
LucasCarrizo14 <lucas.maxi.c@gmail.com>
2026-04-17 00:59:17 -0300

    feat: Agregue nuevas estructuras en el archivo de ejercicios en Javascrip y su Html referente

--- javascript/EjerciosIf.js
+++ javascript/EjerciosIf.js
@@ -1,3 +1,5 @@
+//IF
+
 /* Ejercicio 1: if simple
 Declara una variable edad y muestra por consola "Es mayor de edad" si es mayor o igual a 18. */
 let edad = 19;
@@ -52,3 +54,143 @@ Declara una variable edad y otra variable esMayor que sea true si la edad es may
 let edad1 = 19;
 let Esmayor = edad >= 18;
 console.log(Esmayor);
+
+//FOR
+
+/*Ejercicio 1: for clásico
+Utiliza un for para mostrar por consola los números del 1 al 5.*/
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+/*Ejercicio 2: while
+Usa un while para mostrar por consola los números del 5 al 1 (en orden descendente).*/
+
+let numerow = 5;
+while (numerow >= 1) {
+  console.log(numerow);
+  numerow--;
+}
+
+/*Ejercicio 3: do...while
+Usa un do...while para mostrar por consola los números del 1 al 3.*/
+
+let numerosdw = 1;
+do {
+  console.log(numerosdw);
+  numerosdw++;
+} while (numerosdw <= 3);
+
+/*Ejercicio 4: Recorrer un array con for...of
+Crea un array animales con "perro", "gato" y "loro". Usa for...of para mostrar cada animal por consola.*/
+
+const animales = ["perro", "gato", "loro"];
+for (const animal of animales) {
+  console.log(animal);
+}
+
+//ARRAYS
+
+/* Ejercicio 1: Acceder y modificar elementos
+Crea un array llamado frutas con los valores "manzana", "banana" y "pera". Cambia el valor "banana" por "naranja" y muestra el array por consola.*/
+
+let frutas = ["manzana", "banana", "pera"];
+frutas[1] = "naranja";
+console.log(frutas);
+
+/* Ejercicio 2: Métodos push y pop
+Crea un array numeros con los valores 1, 2 y 3. Agrega el número 4 al final y luego elimina el último elemento. Muestra el array final por consola.*/
+
+let numerosa = [1, 2, 3];
+numerosa.push(4);
+numerosa.pop();
+console.log(numerosa);
+
+/* Ejercicio 3: Concatenar arrays
+Crea dos arrays: animales1 con "perro" y "gato", y animales2 con "loro" y "pez". Une ambos arrays en uno solo llamado animales y muéstralo por consola.*/
+
+let animales1 = ["perro", "gato"];
+let animales2 = ["loro", "pez"];
+let animalesu = animales1.concat(animales2);
+console.log(animalesu);
+
+/* Ejercicio 4: Dar vuelta un array
+Crea un array numeros con los valores 1, 2, 3, 4, 5. Usa un método para invertir el orden de los elementos y muestra el resultado por consola.*/
+
+let numerosinv = [1, 2, 3, 4, 5];
+numerosinv.reverse();
+console.log(numerosinv);
+
+/* Ejercicio 5: Extraer una parte de un array
+Crea un array colores con "rojo", "verde", "azul", "amarillo", "violeta". Usa slice para obtener un nuevo array con los elementos del índice 1 al 3 (sin incluir el 3) y muéstralo por consola*/
+
+let colores = ["rojo", "verde", "azul", "amarillo", "violeta"];
+let seleccol = colores.slice(1, 3);
+console.log(seleccol);
+
+/* Ejercicio 6: Recorrer un array y crear uno nuevo
+Crea un array edades con los valores 10, 15, 20, 25. Usa map para crear un nuevo array llamado mayores que contenga true si la edad es mayor o igual a 18, y false si no. Muestra el resultado por consola.*/
+
+let edades = [10, 15, 20, 25];
+let mayorese = edades.map((edad) => edad >= 18);
+console.log(mayorese);
+
+/* Ejercicio 7: mostrar el doble de cada elemento de un array (forEach)
+Crea un array numeros con los valores 2, 4, 6, 8. Usa forEach para mostrar el doble de cada número por consola. */
+
+let numerosdob = [2, 4, 6, 8];
+numerosdob.forEach((dobles) => {
+  console.log(dobles * 2);
+});
+
+// OBJETOS
+/*Ejercicio 1: Acceder a propiedades de un objeto
+Crea un objeto llamado auto con las propiedades marca, modelo y año. Muestra por consola el modelo del auto.*/
+
+const auto = {
+  marca: "Fiat",
+  modelo: "600",
+  año: 1988,
+};
+console.log(auto.modelo);
+
+/*Ejercicio 2: Modificar propiedades de un objeto
+Crea un objeto persona con las propiedades nombre y edad. Cambia el valor de la edad y agrega una nueva propiedad profesion.*/
+
+let persona = {
+  nombre: "Juan",
+  edad: 19,
+};
+
+persona.edad = 26;
+persona.profesion = "Abogado";
+console.log(persona);
+
+/*Ejercicio 3: Destructuring de objetos
+Dado el siguiente objeto libro, usa destructuring para extraer las propiedades titulo y autor en variables independientes.*/
+
+let libro = {
+  titulo: "Don Quijote de la Mancha",
+  autor: "Miguel de Cervantes Saavedra",
+  año: 1605,
+};
+
+let { titulo, autor } = libro;
+console.log(titulo);
+console.log(autor);
+
+/* Ejercicio 4: Array de objetos
+Crea un array llamado alumnos que contenga tres objetos, cada uno con las propiedades nombre y nota. Muestra por consola el nombre del alumno con la nota más alta.*/
+let alumnos = [
+  { nomrbre: "Lucas", nota: 8 },
+  { nombre: "Florencia", nota: 7 },
+  { nombre: "Pedro", nota: 9 },
+];
+let mejornot = alumnos[0];
+for (let alumno of alumnos) {
+  if (alumno.nota > mejornot.nota) {
+    mejornot = alumno;
+  }
+}
+console.log(mejornot.nombre);
